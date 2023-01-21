74f6d5fa854d8da0641b444f2a14cfa348c98279
Thomas Hope <contact@thomashope.co.uk>
2023-01-21 00:21:22 +0000

    fix crashing conf file

--- conf.lua
+++ conf.lua
@@ -1,9 +1,9 @@
 function love.conf(t)
-    t.version = '0.10.2'                -- The LÖVE version this game was made for (string)
+    t.version = '11.4'
     t.console = false
 
     t.window.title = 'Sounds in a Dark Room'
-    t.window.icon = 'res/images/icon.png'
+    -- t.window.icon = 'res/images/icon.png'
     t.window.resizable = true
     t.window.vsync = true
 end
