1ec07d3f491bed9170dfbaac1ac51ee45aaf7a08
Thomas Hope <contact@thomashope.co.uk>
2023-01-21 00:56:09 +0000

    fix level select screen

--- src/level_select_state.lua
+++ src/level_select_state.lua
@@ -24,14 +24,8 @@ end
 level_select_state['level'] = function( self, scancode )
 	if scancode == 'space' or controller_1:button_pressed_a() then
 		-- Load the currently selected level and go to playing state
-		Level.load(self.level_list[self.level_index])
+		Level.load(self.level_list[self.index])
 		Gamestate.switch(playing_state)
-	elseif scancode == 'left' or controller_1:button_pressed_left() then
-		self.level_index = self.level_index - 1
-		if self.level_index < 1 then self.level_index = #self.level_list end
-	elseif scancode == 'right' or controller_1:button_pressed_right() then
-		self.level_index = self.level_index + 1
-		if self.level_index > #self.level_list then self.level_index = 1 end
 	end
 end
 
@@ -54,14 +48,30 @@ local function GetFileExtension(str)
 end
 
 function level_select_state:update_level_list()
-	local items = love.filesystem.getDirectoryItems('res/levels')
+	local levels = love.filesystem.getDirectoryItems('res/levels')
+
 	self.level_list = {}
+	self.items = {}
 
 	-- only add PNGs to the level list
-	for i = 1, #items do
-		if GetFileExtension(items[i]) == ".png" then
-			table.insert( self.level_list, items[i] )
+	for i = 1, #levels do
+		print(i, levels[i])
+		if GetFileExtension(levels[i]) == ".png" then
+			table.insert( self.items, 'level' )
+			table.insert( self.level_list, levels[i] )
 		end
+	end
+
+	table.insert( self.items, 'return' )
+end
+
+function level_select_state:get_return_menu_item_string()
+	if self.previous == main_menu_state then
+		return 'return to main menu'
+	elseif self.previous == playing_state then
+		return 'return to game'
+	elseif self.previous == pause_menu_state then
+		return 'return to pause menu'
 	end
 end
 
@@ -75,18 +85,12 @@ function level_select_state:draw()
 		local item = self.items[i]
 
 		if item == 'level' then
-			item = GetFileNameWithoutExtension(self.level_list[self.level_index])
+			item = GetFileNameWithoutExtension(self.level_list[i])
 		end
 
 		-- Indicate where we will be returning to
 		if item == 'return' then
-			if self.previous == main_menu_state then
-				item = item..' to main menu'
-			elseif self.previous == playing_state then
-				item = item..' to game'
-			elseif self.previous == pause_menu_state then
-				item = item..' to pause menu'
-			end
+			item = self:get_return_menu_item_string()
 		end
 
 		-- prepend > to indicate the selected item
